97e42c932fc303d19e4bd9f21a0395bfbe23d11c
Constant Druon <druon.constant@gmail.com>
2022-06-13 21:02:22 +0200

    feat: use a button instead of a switch to toggle theme

--- src/components/DarkModeSwitch/index.tsx
+++ src/components/DarkModeSwitch/index.tsx
@@ -1,7 +1,17 @@
-import { useColorMode, Switch } from "@chakra-ui/react";
+import React from "react";
+import { useColorMode, IconButton } from "@chakra-ui/react";
+import { IoSunny, IoMoon } from "react-icons/io5";
 
 export const DarkModeSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === "dark";
-  return <Switch size="lg" isChecked={isDark} onChange={toggleColorMode} />;
+
+  return (
+    <IconButton
+      aria-label="switch color mode"
+      size="md"
+      icon={isDark ? <IoSunny /> : <IoMoon />}
+      onClick={toggleColorMode}
+    />
+  );
 };
